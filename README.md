576738d9a8043b33ce167fa48d3b2aaa6306173b
Daniel Kling <dan13lkl1ng@googlemail.com>
2018-06-07 00:40:46 +0200

    Create Readme. Create database

---
+++ README.md
@@ -0,0 +1,16 @@
+# Item Catalog App
+Made during Google Scholarship of Full Stack Web Developer Nanodegree Program at Udacity - 2018
+
+This project sets up a sqlite database for an item catalog app. The provided Python script uses the sqlalchemy library to query the database.
+## Requirements
+1. Python 3
+2. sqlite
+3. python-sqlalchemy
+4. flask
+
+## Getting started
+1. Create SQLite Database
+```
+$ python database_setup.py
+```
+
---
+++ database_setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(1000))
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    item = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+                'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'cat_id': self.cat_id
+            }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
