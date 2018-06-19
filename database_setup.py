2cb62baa4d53c7f8040354d389f709f3a1b38e27
Daniel Kling <dan13lkl1ng@googlemail.com>
2018-06-19 21:47:22 +0200

    add templates and authentication

--- database_setup.py
+++ database_setup.py
@@ -2,9 +2,10 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from datetime import datetime
+from passlib.apps import custom_app_context as pwd_context
 
 
 Base = declarative_base()
@@ -23,6 +24,24 @@ class Category(Base):
                 'name': self.name
                 }
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    #def get_id(self):
+        # returns the user e-mail
+        #return unicode(self.id)
+
+
+
 
 class Item(Base):
     __tablename__ = 'item'
@@ -33,6 +52,8 @@ class Item(Base):
     cat_id = Column(Integer, ForeignKey('category.id'))
     item = relationship(Category)
     datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
 
@@ -44,6 +65,7 @@ class Item(Base):
                 'description': self.description,
                 'cat_id': self.cat_id
             }
+
 
 
 engine = create_engine('sqlite:///catalog.db')
--- view.py
+++ view.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify, request, url_for, abort, g, render_template, flash, redirect
 
-from database_setup import Base, Category, Item #, User, Bagel
+from database_setup import Base, Category, Item , User#, Bagel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, scoped_session, lazyload, joinedload
 from sqlalchemy import create_engine
@@ -19,29 +19,45 @@ Base.metadata.bind = engine
 session = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
 
+
+@auth.verify_password
+def verify_password(username, password):
+    print( "Looking for user %s" % username)
+    user = session.query(User).filter_by(username = username).first()
+    if not user: 
+        print("User not found")
+        return False
+    elif not user.verify_password(password):
+        print("Unable to verfy password")
+        return False
+    else:
+        g.user = user
+        return True
+
+
 @app.route('/')
-def  showLatestItems():
+def showLatestItems():
     #lastItems = session.query.order_by('-id').first()
-    #
-    #bla = session.query(Category).options(lazyload(Item)).all()
 
 
     categories = session.query(Category).all()
 
     lastItems = session.query(Item).order_by(Item.id.desc()).limit(10).all()
 
-    return render_template('index.html', lastItems = lastItems, categories = categories)
+
+    return render_template('index.html', lastItems = lastItems, categories = categories, logged_in = True if g.user else False )
 
 
 @app.route('/item/new/', methods=['GET','POST'])
+@auth.login_required
 def newItem():
     categories = session.query(Category).all()
     #categories = Category.query.all()
     if request.method == 'POST':
-        newItem = Item(title = request.form['title'], cat_id = request.form['category'], description=request.form['description'], datetime = datetime.now())
+        newItem = Item(title = request.form['title'], cat_id = request.form['category'], description=request.form['description'], datetime = datetime.now(), user_id = g.user.id)
         session.add(newItem)
         session.commit()
-        #flash("new item created!")
+        flash("New Item Created!")
         return redirect(url_for('showLatestItems'))
     else:
         return render_template('newItem.html', categories = categories)
@@ -63,12 +79,12 @@ def showItems(cat_id):
 
 
 @app.route('/catalog/<int:item_id>/edit/', methods=['GET','POST'])
+@auth.login_required
 def editItem(item_id):
-    item = session.query(Item).filter_by(id=item_id).one()
+    item = session.query(Item).filter_by(id=item_id).filter_by(user_id=g.user.id).one()
     categories = session.query(Category).all()
     if request.method =='POST':
         #editedItem = Item(
-
 
         #session.execute(update(Item).where(id==item_id).values(title='lorem'))
         #session.commit()
@@ -76,13 +92,57 @@ def editItem(item_id):
             filter(Item.id == item_id).\
             update({"title": request.form['title'],\
             "description": request.form['description'],\
-            "cat_id": request.form['category']})
+            "cat_id": request.form['category'],\
+            "user_id": g.user.id})
         session.commit()
 
         return redirect(url_for('showLatestItems'))
     else:
         return render_template('edit_item.html', item = item, categories = categories)
 
+
+@app.route('/catalog/<int:item_id>/delete/', methods=['GET','POST']) 
+@auth.login_required
+def deleteItem(item_id):
+    itemToDelete = session.query(Item).filter_by(id=item_id).filter_by(user_id=g.user.id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash("Item deleted")
+        return redirect(url_for('showLatestItems'))
+    else:
+        return render_template('deleteItem.html', item=itemToDelete)
+
+
+@app.route('/users', methods = ['POST'])
+def new_user():
+
+    username = request.json.get('username')
+    print(username)
+    password = request.json.get('password')
+    if username is None or password is None:
+        print("missing arguments")
+        abort(400) 
+        
+    if session.query(User).filter_by(username = username).first() is not None:
+        print("existing user")
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+        
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+
+@app.route('/users/<int:id>')
+@auth.login_required
+def get_user(id):
+    user = session.query(User).filter_by(id=id).one()
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
 
 
 if __name__ == '__main__':
